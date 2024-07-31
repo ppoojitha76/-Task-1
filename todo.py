323299efce5bec832681fc3f740a04ed8124a4f2
ppojitha72 <ppoojitha72@gmail.com>
2024-07-31 18:53:38 +0530

    first commit

---
+++ todo.py
@@ -0,0 +1,87 @@
+# 1. import module
+from tkinter import *
+from tkinter import messagebox
+#5. create fuctions
+def newTask():
+    task=my_entry.get()
+    if task != "":
+        lb.insert(END, task)
+        my_entry.delete(0, "end")
+    else:
+        messagebox.showwarning("warning","please enter some task.")
+def deleteTask():
+    lb.delete(ANCHOR)
+#2.configur& create mainwindow
+ws = Tk()
+ws.geometry('500x400+500+200')
+ws.title('PythonGuides')
+ws.config(bg='#223441')
+ws.resizable(width=False, height=False)
+
+#4.create widgets (frame,listbox, scrollbar,Entry,button)
+frame = Frame(ws)
+frame.pack(pady=10)
+
+lb = Listbox(
+    frame,
+    width=25,
+    height=8,
+    font=('Times', 18), 
+    bd=0,
+    fg='#464646',
+    highlightthickness=0,
+    selectbackground='#a6a6a6',
+    activestyle="none",
+ )
+lb.pack(side=LEFT, fill=BOTH)
+
+task_list ={
+    'Eat apple',
+    'drink water',
+    'go gym',
+    'software',
+    'take nap',    'learn something',
+     'paint canvas',
+}
+for item in task_list:
+    lb.insert(END,item)
+
+sb=Scrollbar(frame)
+sb.pack(side=RIGHT,fill=BOTH)
+
+lb.config(yscrollcommand=sb.set)
+sb.config(command=lb.yview)
+
+my_entry=Entry(
+    ws,
+    font=('time',24)
+
+)
+my_entry.pack(pady=20)
+button_frame=Frame(ws)
+button_frame.pack(pady=20)
+
+addTask_btn=Button(
+    button_frame,
+    text='Add Task',
+    font=('times 14'),
+    bg='#c5f776',
+    padx=20,
+    pady=10,
+
+)
+addTask_btn.pack(fill=BOTH,expand=True,side=LEFT)
+
+delTask_btn=Button(
+    button_frame,
+    text='Delete Task',
+    font=('times 14'),
+    bg='#ff8b61',
+    padx=20,
+    pady=10,
+
+)
+delTask_btn.pack(fill=BOTH, expand=True, side=LEFT)
+
+# 3. create mainloop
+ws.mainloop()
